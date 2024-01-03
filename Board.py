77c0e33c25437469710f21a00124678738017ece
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-01-04 00:33:42 +0200

    Update Board.py

--- Board.py
+++ Board.py
@@ -2,8 +2,97 @@ from Piece import Piece
 import copy
 
 class Board:
+    
     def __init__(self):
         self.grid = [[None for _ in range(4)] for _ in range(4)]
+        self.left_player = player1
+        self.right_player = player2
+        self.holding_piece = Piece(0,0,None,0,None)
+        self.board = [[None for _ in range(4)] for _ in range(4)]
+        self.turn = "l"
+        self.holding_position = None
+        self.piece_from_board = False
+
+    def current_piece(self,row, col, win):
+        if self.piece_from_board == True:
+            return
+        if((col ==0) and row<3 and self.turn == "l" ):
+            if row == 0 and self.left_player.stack1 != None:
+                self.holding_piece.copy_piece(self.left_player.stack1)
+            elif row == 1 and self.left_player.stack2 != None:
+                self.holding_piece.copy_piece(self.left_player.stack2)
+            elif row == 2 and self.left_player.stack3 != None:
+                self.holding_piece.copy_piece(self.left_player.stack3)
+        elif((col == COLS-1) and row<3 and self.turn == "r"):
+            if row == 0 and self.right_player.stack1 != None:
+                self.holding_piece.copy_piece(self.right_player.stack1)
+            elif row == 1 and self.right_player.stack2 != None:
+                self.holding_piece.copy_piece(self.right_player.stack2)
+            elif row == 2 and self.right_player.stack3 != None:
+                self.holding_piece.copy_piece(self.right_player.stack3)
+        if self.holding_piece.size != 0:
+            self.draw_deck(win)
+            self.holding_position = row
+            self.holding_piece.draw(win)
+    
+    def add_piece_on_board(self,row,col,win):    
+        if self.board[row][col-1] != None and self.holding_piece.size ==0 and((self.board[row][col-1].color ==self.left_player.color and self.turn == "l")or(self.board[row][col-1].color ==self.right_player.color and self.turn == "r")) : 
+            self.piece_from_board = True
+            self.holding_piece.copy_piece(self.board[row][col-1])
+            self.holding_piece.draw(win)
+            self.holding_piece = self.board[row][col-1]
+            # print(self.holding_piece.row,self.holding_piece.col)
+            # self.board[row][col-1] = None
+            return
+        
+        if self.piece_from_board == True:
+            
+            result =self.move_piece((self.holding_piece.row,self.holding_piece.col-1),(row,col-1))
+            print(self.board)
+            if result == "Placed":
+                self.piece_from_board = False
+                # self.holding_piece.color = BLACK
+                # self.holding_piece.draw(win)
+                self.holding_piece = Piece(0,0,None,0,None)
+                self.holding_position = None
+                self.switch_turn()
+                self.print_board(win)
+                self.draw_deck(win)
+                
+        #case playing from hand
+        if self.piece_from_board== False:
+            if self.holding_piece.size == 0:
+                return
+            # print(row,col-1)
+            result = self.place_piece(self.holding_piece,(row,col-1),'hand')
+            if result == "Placed":
+                if self.turn == "l":
+                    self.left_player.update_stack("stack"+str(self.holding_position+1))
+                elif self.turn == "r":
+                    self.right_player.update_stack("stack"+str(self.holding_position+1))
+                else:
+                    print("Error")
+                self.holding_piece.color = BLACK
+                self.holding_piece.draw(win)
+                self.holding_piece = Piece(0,0,None,0,None)
+                self.holding_position = None
+                self.switch_turn()
+                self.print_board(win)
+                self.draw_deck(win)
+        
+        if result.endswith('wins'):
+            self.print_board(win)
+            self.draw_deck(win)
+            return result
+
+    def switch_turn(self):
+        if self.turn == "l":
+            self.turn = "r"
+        else:
+            self.turn = "l"
+            
+    ############################################################################################
+        
 
     def place_piece(self, piece, position, playingPlace):
         row, col = position
