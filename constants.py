d194fd48bf35c724068bbd791133911eef56eabd
Omar Mohammad <omarabdalqawi@gmail.com>
2024-01-04 00:14:27 +0200

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,11 @@
+WIDTH, HEIGHT = 600, 400
+ROWS, COLS = 4, 6
+SQUARE_SIZE = WIDTH//COLS
+
+# rgb
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+second_player = (0, 0, 255)
+GREY = (128,128,128)
+first_player = (0,255,0)
