0674f9afb18ea923d1acf12f5181216ba766ad60
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-01-27 07:21:59 +0200

    Update AI.py

--- AI.py
+++ AI.py
@@ -27,27 +27,40 @@ def minimax(board, depth, max_player):
 ####################################################################### Iterative #####################################################
 
 ########################################################################################################################################
+
 def alpha_beta_pruning(board, depth, max_player, start_time = None , time_limit= None , alpha = float('-inf'), beta = float('inf')):
+    #stop search if time exceeds limit [iterative deepening]
     if  (start_time != None and ((time.time() - start_time) > time_limit)):
         return None, None
+
+    #if goal is found, then return it.
     if board.check_winner(board.board) != 'No Winner':
         return board.SCORE, board
+
+    #evaluate leaf nodes
     if (depth == 0) :
         return board.SCORE, board
+
+    #Maxmizer turn
     if max_player:
         maxEval = float('-inf')
         best_move = None
 
+        #iterate on all valid moves for current state
         for move in board.valid_moves(board , first_player , board.left_player , board.right_player ,True ):
-            # move.display_board()
             evaluation = alpha_beta_pruning(move, depth-1, False,start_time , time_limit, alpha, beta)[0]
+
+            #iterative deepening case
             if evaluation == None:
                 return None, None
+                
             maxEval = max(maxEval, evaluation)
+            
             # Alpha Beta Pruning step
             alpha = max(alpha, maxEval)
             if beta <= alpha:
                 break
+                
             if maxEval == evaluation:
                 best_move = move               
         return maxEval, best_move
@@ -55,15 +68,22 @@ def alpha_beta_pruning(board, depth, max_player, start_time = None , time_limit=
     else:
         minEval = float('inf')
         best_move = None
+        
+        #iterate on all valid moves for current state
         for move in board.valid_moves(board , second_player , board.left_player , board.right_player ,False ):
             evaluation = alpha_beta_pruning(move, depth-1, True, start_time , time_limit, alpha, beta)[0]
+
+            #iterative deepening case
             if evaluation == None:
                 return None, None
+                
             minEval = min(minEval, evaluation)
+            
             # Alpha Beta Pruning step
             beta = min(beta, minEval)
             if beta <= alpha:
                 break
+            
             if minEval == evaluation:
                 best_move = move             
         return minEval, best_move
