70f493fbad14135130750788ddc91dbd08e00beb
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-01-27 07:04:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import pygame
-from constants import WIDTH, HEIGHT, SQUARE_SIZE, RED,first_player,second_player
+import sys
+import pygame
+from constants import WIDTH, HEIGHT, SQUARE_SIZE,first_player,second_player, WHITE, GREY, BLACK
 from Game import Game
 import Player
-FPS = 60
-
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Gobblet Gobblers')
+import sys
 
 def get_row_col_from_mouse(pos):
     x, y = pos
@@ -13,30 +12,40 @@ def get_row_col_from_mouse(pos):
     col = x // SQUARE_SIZE
     return row, col
 
-def main():
+def create_game(option, sub_option = None):
+    if option == "Human vs Human":
+        game = Game(WIN,Player.Player(first_player),Player.Player(second_player))
+    elif option == "Computer vs Computer":
+        game = Game(WIN,Player.Player(first_player, True , "Difficult"),Player.Player(second_player, True , "Difficult"))    
+    elif option == "Human vs Computer":
+        if sub_option == "Easy":
+            game = Game(WIN,Player.Player(first_player),Player.Player(second_player, True))    
+        elif sub_option == "Medium":
+            game = Game(WIN,Player.Player(first_player),Player.Player(second_player, True, "Medium"))    
+        elif sub_option == "Difficult":
+            game = Game(WIN,Player.Player(first_player),Player.Player(second_player, True, "Difficult"))
+    return game        
+
+def run_game(option, sub_option = None):
     run = True
     clock = pygame.time.Clock()
-    game = Game(WIN,Player.Player(first_player),Player.Player(second_player))
-    
+    game = create_game(option, sub_option)    
     game.update()
-    
     while run:
         clock.tick(FPS)
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #pygame.quit()
                 run = False
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                row, col = get_row_col_from_mouse(pos)
-                res = game.select(row, col, WIN)
+                sys.exit() 
+            else:
+                res = game.fire(event)
+                # print(res)
                 if res == "new game":
-                    game = Game(WIN,Player.Player(first_player),Player.Player(second_player))
+                    game = create_game(option, sub_option)
                 elif res == "main menu":
-                    pass
-                # print(row, col)
-                game.update()
-    
-    pygame.quit()
 
-main()
+                    return 
+                game.update()
+    #pygame.quit()          
+####################################################################
