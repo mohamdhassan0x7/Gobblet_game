d0b5f4702d73084d0d2ffde1e4984119d39ba60c
ahmedessamrizk <105478629+ahmedessamrizk@users.noreply.github.com>
2024-01-27 07:27:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,14 @@ from Game import Game
 import Player
 import sys
 
+#get coordinates of the select
 def get_row_col_from_mouse(pos):
     x, y = pos
     row = y // SQUARE_SIZE
     col = x // SQUARE_SIZE
     return row, col
 
+#based on mode ==> instaniate object of game with needed parameters
 def create_game(option, sub_option = None):
     if option == "Human vs Human":
         game = Game(WIN,Player.Player(first_player),Player.Player(second_player))
@@ -35,17 +37,19 @@ def run_game(option, sub_option = None):
         clock.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                #pygame.quit()
                 run = False
                 sys.exit() 
             else:
                 res = game.fire(event)
-                # print(res)
+                #based on return value:
+                #new game  ==> create game with same option and sub options.
+                #main menu ==> return to main screen
+                
                 if res == "new game":
                     game = create_game(option, sub_option)
                 elif res == "main menu":
 
                     return 
-                game.update()
-    #pygame.quit()          
+                #update gui
+                game.update()    
 ####################################################################
