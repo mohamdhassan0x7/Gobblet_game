9c0a83f46d4b8d0a003ebcf5ae343f69a2c62398
Omar Mohammad <omarabdalqawi@gmail.com>
2024-01-27 15:15:16 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,11 +1,18 @@
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 800, 600
 ROWS, COLS = 4, 6
 SQUARE_SIZE = WIDTH//COLS
 
 # rgb
-RED = (255, 0, 0)
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-second_player = (0, 0, 255)
-GREY = (128,128,128)
-first_player = (0,255,0)
+ZERO_Y = 40
+DrawCase = (247, 231, 206)
+DrawReleased = (247, 231, 206)
+DrawPressed =  (153, 88, 42)
+RED = (87,44,25) 
+WHITE = (165,107,66)
+BLACK = (153, 88, 42)
+DARK_BLACK = (170, 88, 42)
+GREY = (67, 40, 24)
+GREY_light = (67, 40, 24 , 128)
+TURN = (111, 29, 27)
+second_player = (165,107,66)
+first_player = (87,44,25)
